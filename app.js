3f07b2a11b802a02ba6bc3b811d14d24a9b4b63f
miraz-ezaz <miraz.ezaz@gmail.com>
2024-07-29 01:25:46 +0600

    update port number

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const hotelRoutes = require('./routes/hotel');
-
+const hotelRoutes = require("./routes/hotel");
 
 app.use(cors());
-app.use('/hotel', hotelRoutes);
+app.use("/hotel", hotelRoutes);
 
 // Handle 404 for unknown routes
 app.use((req, res) => {
-    res.status(404).json({ error: 'Not Found' });
+  res.status(404).json({ error: "Not Found" });
 });
 
 // Start the server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
